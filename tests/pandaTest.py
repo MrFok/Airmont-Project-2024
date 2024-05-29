ab7875738c1a7f808c91e1804c6e5b74150ef1cc
uuriah <132951774+uuriah@users.noreply.github.com>
2024-05-29 10:30:43 -0700

    Change parameter for read excel function to the relative path for the excel sheet

--- tests/pandaTest.py
+++ tests/pandaTest.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_excel('musicTimes.xlsx')
+df = pd.read_excel('tests/musicTimes.xlsx')
 
 print(df.describe())
 
