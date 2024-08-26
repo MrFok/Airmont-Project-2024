5716abe57865c5e824d2e03779846c3c4c9fabac
uuriah <132951774+uuriah@users.noreply.github.com>
2024-08-25 18:14:08 -0700

    Add recommend path

--- webApp/mysite/ocarina/urls.py
+++ webApp/mysite/ocarina/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from .views import SongRecommendationView
 from . import views
 
 urlpatterns = [
+    path('recommend/', SongRecommendationView.as_view(), name = 'song-recommendation'),
     path("", views.index, name="index"),
 ]
